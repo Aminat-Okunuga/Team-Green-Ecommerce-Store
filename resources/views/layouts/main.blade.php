f5fb92bad3f15694c2671673b74e4fce0b6a5bef
Aminat-Okunuga <makadeaminat@gmail.com>
2021-11-29 23:32:35 +0100

    Modified Footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2414,7 +2414,7 @@
 					<div class="row">
 						<div class="col-lg-6 col-12">
 							<div class="left">
-								<p>Copyright © 2020 <a href="http://www.wpthemesgrid.com" target="_blank">Wpthemesgrid</a>  -  All Rights Reserved.</p>
+								<p>Copyright © 2020 <a href="#" target="_blank">TeamGreen</a>  -  All Rights Reserved.</p>
 							</div>
 						</div>
 						<div class="col-lg-6 col-12">
