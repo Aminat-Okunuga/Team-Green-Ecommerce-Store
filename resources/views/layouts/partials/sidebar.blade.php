41c3a70e2879da3cb16210af92f468efa193ba4d
Ahmed Ghaji <mohammedghaji@gmail.com>
2021-11-29 15:50:37 +0100

    Push All
    From me

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -7,7 +7,7 @@
       <ul class="mt-6">
         <li class="relative px-6 py-3">
           <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
-          <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100" href="index-2.html"
+          <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100" href="{{ url('/dashboard') }}"
           >
             <svg
               class="w-5 h-5"
@@ -257,7 +257,7 @@
           ></span>
           <a
             class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-            href="index-2.html"
+            href="{{ url('/dashboard') }}"
           >
             <svg
               class="w-5 h-5"
